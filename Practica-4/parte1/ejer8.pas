282a8fcb1a740392ba8f1a8b20a145591f7f9dcd
Gonzalo Nicolini <gonzalonicolini95@gmail.com>
2026-05-29 22:40:47 -0300

    ejer 8 p4p1 completo

--- Practica-4/parte1/ejer8.pas
+++ Practica-4/parte1/ejer8.pas
@@ -50,3 +50,49 @@ begin
     end;
 end;
 
+procedure masViejos(a:alumnos; var max1, max2: integer; var nom1, nom2,ape1,ape2: string);
+begin
+  if (a.anionac < max1) then
+    begin
+        max2 := max1;
+        nom2 := nom1;
+        ape2 := ape1;
+        max1 := a.anionac;
+        nom1 := a.nombre;
+        ape1 := a.apellido;
+    end
+  else 
+    if (a.anionac < max2) then
+    begin
+        max2 := a.anionac;
+        nom2 := a.nombre;
+        ape2 := a.apellido;
+    end;
+end;
+
+procedure imprimir(v:vector);
+var
+    i,cantPar,max1, max2: integer;
+    nom1, nom2, ape1, ape2: string;
+begin
+    cantPar := 0;
+    max1 := 9999;
+    max2 := 9999;
+    for i := 1 to dimf do
+    begin
+        if (soloPar(v[i].dni)) then
+            cantPar := cantPar + 1;
+        masViejos(v[i], max1, max2, nom1, nom2, ape1, ape2);
+    end;
+    writeln('el porcentaje de alumnos con DNI compuesto solo por digitos pares es: ', ((cantPar/dimf)*100):0:2, '%');
+    writeln('Los dos alumnos mas viejos son: ', nom1, ' ', ape1, ' y ', nom2, ' ', ape2);
+end;
+
+// programa principal
+var
+    v: vector;
+begin
+
+    cargarVector(v);
+    imprimir(v);
+end.
