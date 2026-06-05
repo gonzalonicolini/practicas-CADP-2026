2c32a7870158c2baa6b0a55a291d55d176d9ac06
Gonzalo Nicolini <gonzalonicolini95@gmail.com>
2026-06-05 15:00:15 -0300

    ejer4 arrancando p4p2

---
+++ Practica-4/parte2/ejer4.pas
@@ -0,0 +1,10 @@
+program ejer4;
+const
+    dimf = 1000;
+type
+    alumno = record
+        legajo: integer;
+        nombre: string;
+        apellido: string;
+        asistencia: integer;
+    end;
